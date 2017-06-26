e55c8450b9dadacffa866315868580a84c2c05b2
ChenYuelun <911172573@qq.com>
2017-06-26 11:34:50 +0800

    111

---
+++ app/src/main/java/com/example/p2p/fragment/InvestFragment_Hot.java
@@ -0,0 +1,25 @@
+package com.example.p2p.fragment;
+
+import com.example.p2p.R;
+import com.example.p2p.base.BaseFragment;
+
+/**
+ * Created by chenyuelun on 2017/6/24.
+ */
+
+public class InvestFragment_Hot extends BaseFragment {
+    @Override
+    protected String getUrl() {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_invest_hot;
+    }
+}
